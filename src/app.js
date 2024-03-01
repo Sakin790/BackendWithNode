397098070522577fbb350241e6cb7af8de1ee2e8
Sakin790 <sakinislam79@gmail.com>
2024-03-02 01:04:26 +0600

    utils

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(
+  express.urlencoded({ extended: true, limit: "16kb" })
+); /*url theke data neyar jonno
+extended=> for nested*/
+app.use(express.static("public")); // static file rakhar jonno
+app.use(cookieParser()); // CURD in cookie
+
+
 
 export { app };
