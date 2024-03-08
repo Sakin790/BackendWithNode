a334fed9cf25030e12b4fd91d3dee6464194b922
Sakin790 <sakinislam79@gmail.com>
2024-03-08 15:48:52 +0600

    internet connection neeed

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,7 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
 
-    fs.unlinkSync(localFilePath);
+     fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     //fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
